8c9ed0b6188c433ddc804b0f2f245d305e83b8c6
algonacci <algonacci1@gmail.com>
2022-05-04 13:20:45 +0700

    updates

---
+++ 04_Inisialisasi_dan_Assignment_Nilai_Bilangan_Riil .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float f = 0.555;
+    float x = 1.5E3;
+    
+    printf("Ini nilai f: %f \n", f);
+    printf("Ini nilai f: %5.2f \n", f);
+    printf("Ini nilai x: %10.2f \n", x);
+    return 0;
+}
---
+++ 05_Pengisian_Nilai_Bertipe_Karakter .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char c = 65;
+    char c1;
+    
+    printf("Karakter = %c\n", c);
+    c1 = 'Z';
+    
+    printf("Karakter dalam kode ASCII = %d\n", c);
+    printf("Karakter sebagai huruf = %c\n", c);
+    printf("Karakter dalam kode ASCII = %d\n", c1);
+    printf("Karakter sebagai huruf = %c\n", c1);
+    return 0;
+}
