e60f6602df0fd84427a00c155219c37ff0b9bcb5
Eric Julianto <algonacci1@gmail.com>
2023-03-11 09:40:17 +0700

    Update 32_Selection.c

--- 32_Selection.c
+++ 32_Selection.c
@@ -2,5 +2,17 @@
 
 int main()
 {
-    return 0;
+	//	int angka = 5;
+	//	switch(angka) {
+	//		case 1:
+	//			printf("satu\n");
+	//			break;
+	//		case 5:
+	//			printf("lima\n");
+	//			break;
+	//	}
+
+	int nilai = 2;
+	printf("%s", (nilai % 2 == 0 ? "genap" : "ganjil"));
+	return 0;
 }
